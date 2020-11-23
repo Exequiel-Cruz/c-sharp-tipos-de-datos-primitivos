0bf82601a85f5db1ba896f924f0762686c78e547
Exequiel Cruz <exequiel.cruz111@gmail.com>
2020-11-23 14:12:18 -0300

    Agregar archivos de proyecto.

---
+++ Tipos de datos primitivos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipos_de_datos_primitivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
